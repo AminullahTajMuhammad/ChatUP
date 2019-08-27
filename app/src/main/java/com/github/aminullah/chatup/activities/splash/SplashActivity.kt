a66ce834179070a9e2f5a8acffbefa873418f463
Aminullah Taj Muhammad <aminullahtajmuhammad@gmail.com>
2019-08-28 02:01:04 +0500

    Added Application Logo and Splash Screen

---
+++ app/src/main/java/com/github/aminullah/chatup/activities/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.github.aminullah.chatup.activities.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.aminullah.chatup.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
