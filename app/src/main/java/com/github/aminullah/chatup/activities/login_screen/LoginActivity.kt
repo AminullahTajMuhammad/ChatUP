f1fb25d28b44c24154f3eebd00d4b451a12b0a5e
Aminullah Taj Muhammad <aminullahtajmuhammad@gmail.com>
2019-08-31 14:52:22 +0500

    Added Login Screen UI and functionality

--- app/src/main/java/com/github/aminullah/chatup/activities/login_screen/LoginActivity.kt
+++ app/src/main/java/com/github/aminullah/chatup/activities/login_screen/LoginActivity.kt
@@ -2,12 +2,49 @@ package com.github.aminullah.chatup.activities.login_screen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.github.aminullah.chatup.R
+import com.github.aminullah.chatup.utils.gone
+import com.github.aminullah.chatup.utils.visible
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        setupListeners()
+    }
+
+    fun setupListeners() {
+        btnLogin.setOnClickListener {
+            btnLogin.gone()
+            loginProgress.visible()
+
+            Handler().postDelayed({
+                btnLogin.visible()
+                loginProgress.gone()
+            }, 3000)
+        }
+
+        btnIcon.setOnClickListener {
+            btnLogin.gone()
+            loginProgress.visible()
+
+            Handler().postDelayed({
+                btnLogin.visible()
+                loginProgress.gone()
+            }, 3000)
+        }
+
+        btnText.setOnClickListener {
+            btnLogin.gone()
+            loginProgress.visible()
+
+            Handler().postDelayed({
+                btnLogin.visible()
+                loginProgress.gone()
+            }, 3000)
+        }
     }
 }
--- app/src/main/java/com/github/aminullah/chatup/utils/Ktx.kt
+++ app/src/main/java/com/github/aminullah/chatup/utils/Ktx.kt
@@ -2,6 +2,7 @@ package com.github.aminullah.chatup.utils
 
 import android.content.Context
 import android.content.Intent
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 fun AppCompatActivity.gotoActivity(context: Context, cls: Class<*>) {
@@ -10,4 +11,15 @@ fun AppCompatActivity.gotoActivity(context: Context, cls: Class<*>) {
 fun AppCompatActivity.gotoActivityWithFinih(context: Context, cls: Class<*>) {
     this.startActivity(Intent(context, cls))
     this.finish()
+}
+
+// Visibility extensions
+fun View.visible() {
+    this.visibility = View.VISIBLE
+}
+fun View.invisible() {
+    this.visibility = View.INVISIBLE
+}
+fun View.gone() {
+    this.visibility = View.GONE
 }
